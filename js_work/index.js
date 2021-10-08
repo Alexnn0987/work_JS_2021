1dcfde0495014fdb0ebc381024717383a8f51c64
Alexey Popov <alexnn0987@mail.ru>
2021-10-08 22:10:31 +0300

    Практика1

--- js_work/index.js
+++ js_work/index.js
@@ -1,2 +1,17 @@
-console.log('Привет из NodeJS');
-const name = 'Alex 666';
+// console.log('Привет из NodeJS');
+// const name = 'Alex 666';
+
+// let value = '2' * 2 + '2';
+// console.log(typeof value, value);
+
+// let value1 = '2' + 2 * '2';
+// console.log(typeof value1, value1);
+
+// let value2 = 4 + 5 + '0';
+// console.log(typeof value2, value2);
+
+// let value3 = ' -4 ' / 0 + 1;
+// console.log(typeof value3, value3);
+
+// let value4 = ' -4 ' / 1;
+// console.log(typeof value4, value4);
