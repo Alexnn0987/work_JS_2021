b9b18b0d818d19765036a5ca9b57013271c486aa
Alexey Popov <alexnn0987@mail.ru>
2021-10-08 19:18:51 +0300

    Практика

---
+++ js_work/lesson2_3/index.js
@@ -0,0 +1,32 @@
+// let value = '2' - 2 * '5';
+// console.log(typeof value, value);
+
+// let value1 = '2' + 2 * '5';
+// console.log(typeof value1, value1);
+
+// let value2 = 2 + 2;
+// console.log(typeof value2, value2);
+
+// let value3 = '2' + 2;
+// console.log(typeof value3, value3);
+
+// let value6 = 2 + '2';
+// console.log(typeof value6, value6);
+
+// let value4 = 2 - 2;
+// console.log(typeof value4, value4);
+
+// let value5 = '2' - 2;
+// console.log(typeof value5, value5);
+
+// let value7 = 2 - '2';
+// console.log(typeof value7, value7);
+
+// let value8 = 'y' + 2;
+// console.log(typeof value8, value8);
+
+// let value9 = 2 + 'h';
+// console.log(typeof value9, value9);
+
+// let value = '2' + 'u';
+// console.log(typeof value, value);
