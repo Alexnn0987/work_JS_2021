7be196db3002eeb684147e05d227b0cb090c4441
Alexey <Alexnn0987@mail.ru>
2023-11-15 00:04:55 +0300

    add files of index

--- js_work/lesson2_3/index.js
+++ js_work/lesson2_3/index.js
@@ -1,32 +1,32 @@
-// let value = '2' - 2 * '5';
-// console.log(typeof value, value);
+let value = "2" - 2 * "5";
+console.log(`value - ${typeof value}, ${value}`);
 
-// let value1 = '2' + 2 * '5';
-// console.log(typeof value1, value1);
+let value1 = "2" + 2 * "5";
+console.log(`value1 - ${typeof value1}, ${value1}`);
 
-// let value2 = 2 + 2;
-// console.log(typeof value2, value2);
+let value2 = 2 + 2;
+console.log(`value2 - ${typeof value2}, ${value2}`);
 
-// let value3 = '2' + 2;
-// console.log(typeof value3, value3);
+let value3 = "2" + 2;
+console.log(`value3 - ${typeof value3}, ${value3}`);
 
-// let value6 = 2 + '2';
-// console.log(typeof value6, value6);
+let value4 = 2 + "2";
+console.log(`value4 - ${typeof value4}, ${value4}`);
 
-// let value4 = 2 - 2;
-// console.log(typeof value4, value4);
+let value5 = 2 - 2;
+console.log(`value5 - ${typeof value5}, ${value5}`);
 
-// let value5 = '2' - 2;
-// console.log(typeof value5, value5);
+let value6 = "2" - 2;
+console.log(`value6 - ${typeof value6}, ${value6}`);
 
-// let value7 = 2 - '2';
-// console.log(typeof value7, value7);
+let value7 = 2 - "2";
+console.log(`value7 - ${typeof value7}, ${value7}`);
 
-// let value8 = 'y' + 2;
-// console.log(typeof value8, value8);
+let value8 = "y" + 2;
+console.log(`value8 - ${typeof value8}, ${value8}`);
 
-// let value9 = 2 + 'h';
-// console.log(typeof value9, value9);
+let value9 = 2 + "h";
+console.log(`value9 - ${typeof value9}, ${value9}`);
 
-// let value = '2' + 'u';
-// console.log(typeof value, value);
+let value10 = "2" + "u";
+console.log(`value10 - ${typeof value10}, ${value10}`);
